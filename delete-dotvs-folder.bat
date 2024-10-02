13e564e244b74998f44bf5ba8922715ab716e518
Andrew <itsandrewplayz@hotmail.com>
2024-10-02 22:15:29 +0800

    Updated .vs removing tool

--- delete-dotvs-folder.bat
+++ delete-dotvs-folder.bat
@@ -4,6 +4,8 @@ echo This tool will delete all .vs project folders.
 pause
 echo Deleting...
 rmdir KeyServer\.vs /s /q
+rmdir KeyServer\KeyServer\.vs /s /q
+rmdir KeyServer\KeyServerManager\.vs /s /q
 echo.
 echo Done!
 pause
